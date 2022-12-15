17a89e29f4b1a49b1613e8d5b001373730075736
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2022-12-15 19:33:34 +0530

    test case updated

--- cypress/e2e/qaautomationlabs.cy.js
+++ cypress/e2e/qaautomationlabs.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Dashboard", () => {
+describe("Dashboard", { testIsolation: false }, () => {
   it("Open URL", () => {
     cy.visit("https://qaautomationlabs.com/");
   });
