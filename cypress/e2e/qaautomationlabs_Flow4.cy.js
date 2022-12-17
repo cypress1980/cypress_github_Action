68fc22de51487fa46bce9b59ad978d46fdc43257
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2022-12-17 12:50:32 +0530

    run 5 tc in 1 machine

---
+++ cypress/e2e/qaautomationlabs_Flow4.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe("QAAutomationLabs.com", { testIsolation: false }, () => {
+  it("Open URL", () => {
+    cy.visit("https://qaautomationlabs.com/");
+  });
+  it("Click on Read More ", () => {
+    cy.get(".staticslider-button").click();
+  });
+  it("Verify Particular Blog ", () => {
+    cy.contains(
+      "Running End-to-End Cypress Test cases In Google Cloud Build Pipeline"
+    );
+  });
+  it("Click on Blogs", () => {
+    cy.contains("Blog").scrollIntoView().click({ force: true });
+  });
+  it("Search the datas", () => {
+    cy.get('[id="wp-block-search__input-2"]').scrollIntoView();
+    cy.get('[id="wp-block-search__input-2"]')
+      .click({ force: true })
+      .type("cypress");
+    cy.get('[id="search-icon"]').click({ force: true });
+    cy.contains("Search Results for: cypress");
+  });
+});
